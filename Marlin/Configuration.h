9798caa02cd297b0787cf37b2d3e0c0e990e7b93
ivan.maruca <ivan.maruca@gmail.com>
2020-12-21 15:58:22 +0100

    init

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -72,7 +72,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Dazero.it, Ender-3 SKR V1.4)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Dazero.it, Ender-3 SKR V1.4 TURBO)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -129,11 +129,11 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_BTT_SKR_V1_4
+  #define MOTHERBOARD BOARD_BTT_SKR_V1_4_TURBO
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender-3 SKR V1.4"
+#define CUSTOM_MACHINE_NAME "Ender-3 SKR V1.4 TURBO"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
